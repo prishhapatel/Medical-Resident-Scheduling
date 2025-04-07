c437825a35368136fe2a7ca818000e42be593ca1
Richard Phillips <richardphillipscs@gmail.com>
2025-04-06 22:06:11 -0400

    add website to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { ModeToggle } from "@/components/ui/modetoggle";
-
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
@@ -16,19 +16,61 @@ export default function Home() {
           <h1 className="text-2xl font-bold">PYSCALL</h1>
         </div>
         <div className="flex space-x-10 pr-5">
-          <h1 className="text-2xl font-medium">Contact</h1>
-          <h1 className="text-2xl font-medium">About</h1>
-          <h1 className="text-2xl font-medium">Location</h1>
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+            Contact
+          </Button>
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+            About
+          </Button>
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+            Location
+          </Button>
         </div>
       </header>
+      <div className="flex-grow flex items-center justify-center pt-25 bg-blue-950">
+        <Card className="p-10 bg-white shadow-lg rounded-4xl w-[400px]">
+          <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-lg font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-lg font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <Button className="w-full bg-gray-500 text-white py-3 rounded-md hover:bg-gray-800">
+              Login
+            </Button>
+          </form>
+        </Card>
+      </div>
       <div className="bg-blue-950 flex-grow" />
       <footer className="bg-gray-150 text-black p-2 text-center">
-        <div className="flex justify-center items-center space-x-6 mb-2">
+        <div className="flex justify-center items-center space-x-6 mb-1">
           <Image
-            src="/UCFFooter.jpg"
+            src="/UCF-College-of-Medicine-Footer.png"
             alt="UCF Footer Logo"
-            width={150} 
-            height={75} 
+            width={150}
+            height={75}
             className="object-contain"
           />
           <div className="h-16 w-px bg-gray-400" />
@@ -36,7 +78,7 @@ export default function Home() {
             src="/HCAFooter.png"
             alt="HCA Footer Logo"
             width={150}
-            height={75} 
+            height={75}
             className="object-contain"
           />
         </div>
