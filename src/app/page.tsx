46010f31be749827259bc4fdc9f0ec4863ee1980
Richard Phillips <richardphillipscs@gmail.com>
2025-04-07 11:16:30 -0400

    hi prisha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-gray-150 text-black p-5 flex items-center justify-between border-b border-gray-300 shadow-lg">
+      <header className="bg-gray-100 text-black p-5 flex items-center justify-between border-b border-gray-300 shadow-lg">
         <div className="flex items-center">
           <Image
             src="/HCAHeader.png"
@@ -16,20 +16,20 @@ export default function Home() {
           <h1 className="text-2xl font-bold">PSYCALL</h1>
         </div>
         <div className="flex space-x-10 pr-5">
-          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-md">
             Contact
           </Button>
-          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-md">
             About
           </Button>
-          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-sm">
+          <Button className="text-2xl font-medium bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200 shadow-md">
             Location
           </Button>
         </div>
       </header>
       <div className="flex-grow flex items-center justify-center pt-25 bg-blue-950">
         <Card className="p-10 bg-white shadow-lg rounded-4xl w-[400px]">
-          <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+          <h2 className="text-2xl font-bold mb-4 text-center text-black">Login</h2>
           <form className="space-y-4">
             <div>
               <label
@@ -64,7 +64,7 @@ export default function Home() {
         </Card>
       </div>
       <div className="bg-blue-950 flex-grow" />
-      <footer className="bg-gray-150 text-black p-2 text-center">
+      <footer className="bg-gray-100 text-black p-2 text-center">
         <div className="flex justify-center items-center space-x-6 mb-1">
           <Image
             src="/UCF-College-of-Medicine-Footer.png"
