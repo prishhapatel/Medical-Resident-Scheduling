879886c07ac34fa3b85cd2c991c38c7d97812ff0
Jean Carlos <jeancarloslinlai@gmail.com>
2025-04-07 10:58:59 -0400

    Add project files.

---
+++ Data/Models/MedicalContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MedicalDemo.Data.Models
+{
+
+    public class MedicalContext : DbContext
+    {
+        public DbSet<Admins> admins { get; set; }
+
+        public DbSet<Resident> residents { get; set; }
+
+        public MedicalContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+    }
+}
---
+++ Data/Models/MedicalContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace MedicalDemo.Data.Models
+{
+    // Adjust the namespace to match your actual structure
+    public class MedicalContextFactory : IDesignTimeDbContextFactory<MedicalContext>
+    {
+        public MedicalContext CreateDbContext(string[] args)
+        {
+            // 1. Create the builder
+            var builder = new DbContextOptionsBuilder<MedicalContext>();
+
+            // 2. Configure your connection (hard-coded or from config)
+            builder.UseMySql(
+                "server=135.148.149.147;port=3306;database=sd1;user=developer;password=G13Scheduling;",
+                new MySqlServerVersion(new Version(8, 0, 3)) // or your actual MySQL version
+            );
+
+            // 3. Return a new instance of your DbContext
+            return new MedicalContext(builder.Options);
+        }
+    }
+}
